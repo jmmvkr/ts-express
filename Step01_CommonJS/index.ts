5da0fc224dafb677a82a07a0c6c0153a4c9fae42
jmmvkr <jmmvkr@gmail.com>
2022-05-18 22:00:03 +0800

    Add default port when process.env.PORT is missing

--- Step01_CommonJS/index.ts
+++ Step01_CommonJS/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send(`Express.js Server in TypeScript`);
